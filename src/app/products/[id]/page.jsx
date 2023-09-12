fd95e06586bf40f6699829bd06607e0c669720d7
jihad-hossain1 <jihadkham934@gmail.com>
2023-09-12 11:07:17 +0600

    some error fix

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -1,14 +1,15 @@
 // import { getSingleProduct } from "@/utils/getSingleProduct";
+import "server-only";
 import { getSingleProduct } from "@/utils/getSingleProduct";
 import React from "react";
 
 const SingleProductDetails = async ({ params: { id } }) => {
   const productDetail = await getSingleProduct(id);
-  const { imageUrls, title, price, ratings, features, details, brand } =
-    productDetail;
+  const { title, details } = productDetail;
   return (
     <div>
       <h4 className="text-center text-3xl font-bold">{title}</h4>
+      <p>{details}</p>
     </div>
   );
 };
